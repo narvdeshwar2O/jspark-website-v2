06f0fe2e11186ef38d0bdeecb3fc4bdc9dbc3383
Sam Soni <sam.07soni@gmail.com>
2026-09-11 12:58:44 +0530

    remove the previous component

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,17 @@
 import LoadGate from './components/LoadGate'
-import Header from './components/Header'
 import { CONSOLE_HOLD } from './data/hydraTimings'
 import Hero from './sections/Hero'
 import Hydra from './sections/Hydra'
-import CommandCenter from './sections/CommandCenter'
-import OpsVision from './sections/OpsVision'
-import OpsMind from './sections/OpsMind'
-import OpsUnity from './sections/OpsUnity'
-import Closing from './sections/Closing'
 
 export default function App() {
   return (
     <main>
       <LoadGate />
-      <Header />
       <Hero />
       <Hydra />
       {/* flow spacer covering the console layer's fixed hold past the
           Hydra unpin, so no section content sits behind it (SCENES.md) */}
       <div aria-hidden="true" style={{ height: `${CONSOLE_HOLD * 100}vh` }} />
-      <CommandCenter />
-      <OpsVision />
-      <OpsMind />
-      <OpsUnity />
-      <Closing />
     </main>
   )
 }
